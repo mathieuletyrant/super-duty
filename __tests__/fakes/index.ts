f62c1c61df15d54315d2c2e55a7dc152d215c275
Leo DUMON <leo.dumon@shine.fr>
2025-02-28 13:05:08 +0100

    test(add workflow on pr): add tests GHA workflow on PR checks

--- __tests__/fakes/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './database.repository.fake';
-export * from './rotation.repository.fake';
-export * from './vacation.repository.fake';
-export * from './code.repository.fake';
-export * from './configuration.repository.fake';
